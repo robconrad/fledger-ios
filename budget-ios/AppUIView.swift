c17adc381eb0ae77cde5673d384e092b1c3b13e0
robert.conrad <robert.conrad@coinposit.com>
2015-04-18 16:11:25 -0700

    Black launch screen

---
+++ budget-ios/AppUIView.swift
@@ -0,0 +1,20 @@
+//
+//  AppUIView.swift
+//  budget-ios
+//
+//  Created by Robert Conrad on 4/18/15.
+//  Copyright (c) 2015 TwoSpec Inc. All rights reserved.
+//
+
+import UIKit
+
+
+class AppUIView: UIView {
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        backgroundColor = AppColors.bgHeader
+    }
+    
+}
