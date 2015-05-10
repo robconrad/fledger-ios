7870b842f87b7f5d04c7c482f20d1809eb26bf80
robert.conrad <robert.conrad@coinposit.com>
2015-05-10 11:31:58 -0700

    Drop Quick and Nimble because Quick isn't supported in AppCode

--- Podfile
+++ Podfile
@@ -1,7 +1,2 @@
 pod 'ParseUI'
 
-target 'fledger-iosTests' do
-  use_frameworks!
-  pod 'Quick', '~> 0.3.1'
-  pod 'Nimble', '~> 0.4.2'
-end
--- fledger-iosTests/LocationServiceImplTest.swift
+++ fledger-iosTests/LocationServiceImplTest.swift
@@ -7,117 +7,85 @@
 //
 
 import XCTest
-import Quick
-import Nimble
 import fledger_ios
 
 
-class LocationServiceImplTest: QuickSpec {
-    override func spec() {
-        let db = DatabaseServiceMock()
-        Services.register(DatabaseService.self, db)
+class LocationServiceImplTest: XCTestCase {
+    
+    var db: DatabaseService!
+    var service: LocationServiceImpl<Location>!
+    
+    override func setUp() {
+        super.setUp()
         
-        let service = LocationServiceImpl()
+        let mydb = DatabaseServiceMock()
+        db = mydb
+        db.createDatabaseDestructive()
+        
+        Services.register(DatabaseService.self, mydb)
+    
+        service = LocationServiceImpl()
+    }
 
-        describe("a location service") {
-            let coordinate = CLLocationCoordinate2D(latitude: 10, longitude: 10)
-            let location = Location(id: nil, name: "name", coordinate: coordinate, address: "address", distance: nil)
-            let location2 = location.copy(name: "name2", address: "address2")
-            
-            beforeEach {
-                let deleted: Int = db.locations.delete()!
-                println("deleted: \(deleted)")
-            }
-            
-            describe("its ModelType") {
-                it("is Location") {
-                    expect(service.modelType()) == ModelType.Location
-                }
-            }
-            
-            describe("getting a Location") {
-                context("by id") {
-                    let id = service.insert(location)
-                    let result = service.withId(id!)
-                    expect(location.withId(id)) == result!
-                }
-            }
-            
-            describe("inserting a Location") {
-                context("fresh") {
-                    let id = service.insert(location)
-                    it("returns an id") {
-                        expect(id).notTo(beNil())
-                    }
-                    it("can be retreived") {
-                        expect(service.withId(id!)) == location.withId(id)
-                    }
-                }
-                context("second item") {
-                    let id = service.insert(location2)
-                    it("returns an id") {
-                        expect(id).notTo(beNil())
-                    }
-                    it("can be retreived") {
-                        expect(service.withId(id!)) == location2.withId(id)
-                    }
-                }
-                context("with a duplicate name") {
-                    let id = service.insert(location.copy(name: "unique name"))
-                    it("returns no id") {
-                        expect(id).to(beNil())
-                    }
-                }
-                context("with a duplicate address") {
-                    let id = service.insert(location.copy(address: "unique addy"))
-                    it("returns no id") {
-                        expect(id).to(beNil())
-                    }
-                }
-            }
-            
-        }
+    func testModelType() {
+        XCTAssertEqual(service.modelType(), ModelType.Location)
+    }
+    
+    func testWithId() {
+        let coordinate = CLLocationCoordinate2D(latitude: 10, longitude: 10)
+        let location = Location(id: nil, name: "name", coordinate: coordinate, address: "address", distance: nil)
+        let id = service.insert(location)
+        let result = service.withId(id!)
+        XCTAssertEqual(location.withId(id), result!)
         
-        func testAll() {
-            
-        }
+        let location2 = location.copy(name: "name2", address: "address2")
+        let id2 = service.insert(location2)
+        let result2 = service.withId(id2!)
+        XCTAssertEqual(location2.withId(id2), result2!)
         
-        func testSelect() {
-            
-        }
+        // fail due to duplicate name/address
+        let id3 = service.insert(location2)
+        XCTAssert(id3 == nil)
+    }
+    
+    func testAll() {
         
-        func testCount() {
-            
-        }
+    }
+    
+    func testSelect() {
         
-        func testInsert() {
-            
-        }
+    }
+    
+    func testCount() {
         
-        func testUpdate() {
-            
-        }
+    }
+    
+    func testInsert() {
         
-        func testDelete() {
-            
-        }
+    }
+    
+    func testUpdate() {
         
-        func testInvalidate() {
-            
-        }
+    }
+    
+    func testDelete() {
         
-        func testItemCount() {
-            
-        }
+    }
+    
+    func testInvalidate() {
         
-        func testNearest() {
-            
-        }
+    }
+    
+    func testItemCount() {
         
-        func testCleanup() {
-            // doesn't do anything yet
-        }
+    }
+    
+    func testNearest() {
         
+    }
+    
+    func testCleanup() {
+        // doesn't do anything yet
     }
 
 }
