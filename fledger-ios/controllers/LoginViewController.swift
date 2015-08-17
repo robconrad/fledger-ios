618f79906151aa45c6800019cdbf036dccc5b93f
robert.conrad <robert.conrad@coinposit.com>
2015-08-16 17:20:13 -0700

    Implement LoginViewHelper

--- fledger-ios/controllers/LoginViewController.swift
+++ fledger-ios/controllers/LoginViewController.swift
@@ -11,77 +11,64 @@ import FledgerCommon
 
 
 class LoginViewController: AppUIViewController {
+    
+    lazy var helper: LoginViewHelper = {
+        return LoginViewHelper(self, self)
+    }()
 
     @IBOutlet weak var email: AppUITextField!
     @IBOutlet weak var emailLabel: AppUILabel!
     @IBOutlet weak var password: AppUITextField!
     @IBOutlet weak var passwordLabel: AppUILabel!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var signupButton: UIButton!
     
     override func viewDidLoad() {
-        if ParseSvc().isLoggedIn() {
-            handleSuccess()
-            return
-        }
-        
+        helper.loginFromCache()
         super.viewDidLoad()
     }
     
     @IBAction func login(sender: AnyObject) {
-        if !validateFields() {
-            return
-        }
-        
-        if ParseSvc().login(email.text, password.text) {
-            handleSuccess()
-        }
-        else {
-            passwordLabel.textColor = AppColors.textError()
-        }
+        helper.login()
     }
     
     @IBAction func signup(sender: AnyObject) {
-        if !validateFields() {
-            return
-        }
-        
-        if ParseSvc().signup(email.text, password.text) {
-            handleSuccess()
-        }
-        else {
-            emailLabel.textColor = AppColors.textError()
-        }
-    }
-    
-    private func handleSuccess() {
-        // services can't be registered until a PFUser is logged in
-        ServiceBootstrap.register()
-        
-        let controller = storyboard?.instantiateViewControllerWithIdentifier("mainTabBarController") as! MainTabBarController
-        UIApplication.sharedApplication().delegate!.window!!.rootViewController = controller
-    }
-    
-    private func validateFields() -> Bool {
-        var valid = true
-        
-        emailLabel.textColor = AppColors.text()
-        if count(email.text) < 5 {
-            emailLabel.textColor = AppColors.textError()
-            valid = false
-        }
-        
-        passwordLabel.textColor = AppColors.text()
-        if count(password.text) < 3 {
-            passwordLabel.textColor = AppColors.textError()
-            valid = false
-        }
-        
-        return valid
+        helper.signup()
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         self.view.endEditing(true)
+    }
+    
+}
+
+extension LoginViewController: LoginViewHelperDataSource {
+    
+    func getEmail() -> String {
+        return email.text
+    }
+    
+    func getPassword() -> String {
+        return password.text
+    }
+    
+}
+
+extension LoginViewController: LoginViewHelperDelegate {
+    
+    func notifyEmailValidity(valid: Bool) {
+        emailLabel.textColor = valid ? AppColors.text() : AppColors.textError()
+    }
+    
+    func notifyPasswordValidity(valid: Bool) {
+        passwordLabel.textColor = valid ? AppColors.text() : AppColors.textError()
+    }
+    
+    func notifyLoginResult(valid: Bool) {
+        notifyEmailValidity(valid)
+        notifyPasswordValidity(valid)
+        if valid {
+            let controller = storyboard?.instantiateViewControllerWithIdentifier("mainTabBarController") as! MainTabBarController
+            UIApplication.sharedApplication().delegate!.window!!.rootViewController = controller
+        }
     }
     
 }
